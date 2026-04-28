9f15539d52a8aa08607a4099cd3d1656ff1cc0b6
ndahe  emmanuel <ndaheemmanuel2-lgtm>
2026-04-28 22:09:05 +0100

    correction app.run

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def analysis():
 
 
 # ------------------ LANCEMENT ------------------
-
 if __name__ == "__main__":
-    app.run(debug=false)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
